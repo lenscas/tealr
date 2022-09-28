d4f547c196903e29141340b50ae33306d3ebf0b4
lenscas <lenscas@gmail.com>
2022-09-29 01:50:23 +0200

    add current version of tealr

--- src/lib.rs
+++ src/lib.rs
@@ -37,3 +37,10 @@ pub use tealr_derive::compile_inline_teal;
     feature = "embed_compiler_from_download"
 ))]
 pub use tealr_derive::embed_compiler;
+
+/// Gets the current version of tealr.
+///
+/// Useful when consuming the .json files to check if it is a supported version
+pub fn get_tealr_versions() -> &'static str {
+    env!("CARGO_PKG_VERSION")
+}
