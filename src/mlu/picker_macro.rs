99fffce2dc558305379f6fb8070e774e295eb46f
barsoosayque <shtoshich@gmail.com>
2023-02-12 12:16:59 +0700

    fix TryFrom for i32 on 32bit system

--- src/mlu/picker_macro.rs
+++ src/mlu/picker_macro.rs
@@ -287,6 +287,9 @@ impl_from_exact!(i8, mlua::Value::Integer(x), x, TryFromIntError);
 impl_from_exact!(u8, mlua::Value::Integer(x), x, TryFromIntError);
 impl_from_exact!(i16, mlua::Value::Integer(x), x, TryFromIntError);
 impl_from_exact!(u16, mlua::Value::Integer(x), x, TryFromIntError);
+#[cfg(target_pointer_width = "32")]
+impl_from_exact_non_failing!(i32, mlua::Value::Integer(x), x);
+#[cfg(target_pointer_width = "64")]
 impl_from_exact!(i32, mlua::Value::Integer(x), x, TryFromIntError);
 impl_from_exact!(u32, mlua::Value::Integer(x), x, TryFromIntError);
 impl_from_exact_non_failing!(i64, mlua::Value::Integer(x), x);
